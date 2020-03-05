7e576f99c4b9d8f7b20ff2e37fb45ef099cae22f
Artem Haletsky <ahaletskiy@gmail.com>
2020-03-05 09:25:52 +0300

    reverseLinkedList

--- es6test.js
+++ es6test.js
@@ -5,3 +5,29 @@ window.addEventListener("load", function () {
   const str = `a: ${123}, obj.a: ${obj.a}, time: ${time}`;
   document.getElementById("test").innerHTML = str;
 });
+
+
+function reverseLinkedList(curr, prev) {
+  if (curr == null)
+    return prev;
+  let currNext = curr.next;
+  curr.next = prev;
+  let reversedList = reverseLinkedList(currNext, curr);
+  console.log(reversedList.value);
+  return reversedList;
+}
+
+const linkedList = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+const reversedList = reverseLinkedList(linkedList);

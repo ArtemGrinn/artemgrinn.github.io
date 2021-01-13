4347315a33b837c96f1d53e5a9f41d66988aa2f3
Artem Haletsky <ahaletskiy@gmail.com>
2021-01-13 10:52:20 +0300

    Update es6test.js

--- es6test.js
+++ es6test.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
   let a = 123;  
   let obj = {a};
   let time = new Date();
-  const str = `a: ${123}, obj.a: ${obj.a}, time: ${time}`;
+  const str = `a: ${123}, obj.a: ${obj.a}, time: ${time}, UA: ${navigator.userAgent}`;
   document.getElementById("test").innerHTML = str;
 });
 

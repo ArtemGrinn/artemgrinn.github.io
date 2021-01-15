d6619b1de9895892ff0d9692c481f6bb162bcabb
Artem Haletsky <ahaletskiy@gmail.com>
2021-01-15 09:18:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+window.onerror = function (msg, url, line, col, obj) {
+  alert(msg + '->' + navigator.userAgent + ' -> ' + url + " : " + line + "," + col + " " + (obj ? obj.stack : obj));     
+};
